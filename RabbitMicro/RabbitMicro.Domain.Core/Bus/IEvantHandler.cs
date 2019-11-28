efe5884a8e7297f0c69f91f09cf522f679fd6dc1
laihem.rab@gmail.com <laihem.rab@gmail.com>
2019-11-28 01:36:43 -0500

    Implemented Interfaces and classes for Domain.Core

---
+++ RabbitMicro/RabbitMicro.Domain.Core/Bus/IEvantHandler.cs
@@ -0,0 +1,19 @@
+﻿using RabbitMicro.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMicro.Domain.Core.Bus
+{
+    public interface IEvantHandler<in TEvent> : IEvantHandler
+        where TEvent :Event
+    {
+        Task Handle(TEvent @event);
+    }
+
+    public interface IEvantHandler
+    {
+
+    }
+}
---
+++ RabbitMicro/RabbitMicro.Domain.Core/Commands/Command.cs
@@ -0,0 +1,17 @@
+﻿using RabbitMicro.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitMicro.Domain.Core.Commands
+{
+    public abstract class Command : Message
+    {
+        public DateTime Timestamp { get; set; }
+
+        public Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+    }
+}
